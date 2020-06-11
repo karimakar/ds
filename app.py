4626c7e93e6cc782db0609867e8525cee91a3987
karimakar <35411138+karimakar@users.noreply.github.com>
2020-06-11 23:11:56 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+# load the random forestclassier model MLFOR
+filename = 'mlfor.pkl'
+classifier = pickle.load(open(filename, 'rb'))
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        age = int(request.form['age'])
+        sex = int(request.form['sex'])
+        cp = int(request.form['chestpain'])
+        trestbps = int(request.form['bloodpressure'])
+        chol = int(request.form['chol'])
+        fbs = int(request.form['fbs'])
+        restecg = int(request.form['restecg'])
+        thalach = int(request.form['thalach'])
+        exang = int(request.form['exang'])
+        oldpeak = int(request.form['oldpeak'])
+        slope = int(request.form['slope'])
+        ca = int(request.form['ca'])
+        thal = int(request.form['thal'])
+
+        data = np.array([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
+        my_prediction = classifier.predict(data)
+        return render_template('result.html', prediction=my_prediction)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
